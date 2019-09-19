56a507aacfe0d1bb2be3c87d35c8a65c47a2ac38
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2019-09-19 14:25:04 +0300

    kategorijų crud, nuorodų crud (nebaigta), crud.js tiuninimas skirtingo tipo input'ams (nebaigta)

---
+++ src/main/java/ernadaslinks/PagesController.java
@@ -0,0 +1,28 @@
+package ernadaslinks;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PagesController {
+	
+    @RequestMapping("/")
+    public String greeting() {
+        return "index";
+    }	
+	
+    @GetMapping("/kategorijos")
+    public String kategorijos() {
+        return "kategorijos";
+    }	
+
+    @GetMapping("/nuorodos")
+    public String nuorodos() {
+        return "nuorodos";
+    }    
+}
