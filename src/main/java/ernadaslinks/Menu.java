5405bfbea39aed36ba3ccadecd910b40eccb2e64
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2019-09-25 14:31:19 +0300

    nedideli pakeitimai

--- src/main/java/ernadaslinks/Menu.java
+++ src/main/java/ernadaslinks/Menu.java
@@ -1,5 +1,11 @@
 package ernadaslinks;
 
+/**
+ * @author
+ * by https://www.mkyong.com/java/java-enum-example/
+ * by https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
+ * by https://www.geeksforgeeks.org/enum-in-java/
+ */
 public enum Menu {
 
 	Nuorodos("/nuorodos")
